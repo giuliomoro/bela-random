ec3ee0ec37b6e413512e9aa740bb47d498b2bef4
Giulio Moro <giuliomoro@yahoo.it>
2019-02-13 18:05:22 +0000

    In case of failure, retry by  guessing a more recent URL

--- cross-apt-get-install.sh
+++ cross-apt-get-install.sh
@@ -20,11 +20,62 @@ esac
 
 PACKAGES=$@
 TMP_DIR=/tmp/pkgs/ # FFS, keep the trailing /, or rsync will create a subfolder /tmp/pkgs/pkgs
+TMP_LOG=$TMP_DIR/log
+ONE_LINE_LOG=$TMP_DIR/one_line
+#the expected values in ...+deb9u1_armhf.deb
+ARCH=armhf
+DEB=9
+MAX_DEBU_ATTEMPTS=10
 rm -rf $TMP_DIR
 mkdir -p $TMP_DIR
 cd $TMP_DIR
 # run apt-get install --print-uris on the board. This will return a list of URLs. We then get them with `curl`, copy them to the board and install them.
-ssh $BBB_ADDRESS "rm -rf $TMP_DIR && apt-get install --print-uris $PACKAGES" | { grep -o "'.*'" || { echo "No packages to install" >&2; exit 0; }; }  | xargs -I___ curl -LO ___
+ssh $BBB_ADDRESS "rm -rf $TMP_DIR && apt-get install --print-uris $PACKAGES" | { grep -o "'.*'" || { echo "No packages to install" >&2; exit 0; }; }  | xargs -t -I___ curl -s -w '%{filename_effective} %{url_effective} %{http_code}\n' -LO ___ | tee $TMP_LOG
+grep -q 404 $TMP_LOG || :
+if [ "$?" -eq 0 ]
+then
+	echo
+	echo 'Some packages failed to download, trying to guess a more recent URL'
+	while read line
+	do
+		line=`echo $line | grep " 404" || :`
+		if [ "" != "$line" ]
+		then
+			oldurl=`echo $line | cut -d" " -f 2`
+			echo This failed: $oldurl
+			oldfile=`echo $line | cut -d" " -f 1`
+			rm -rf $oldfile
+			grepped=`echo $oldurl | grep ".*+deb[0-9]\{1,2\}u\([0-9]\{1,\}\)_$ARCH.deb" || :`
+			if [ "$grepped" == "" ]
+			then
+				# no debu : we need to add a fictious one
+				oldurl=`echo $oldurl | sed "s/_$ARCH\.deb/+deb${DEB}u0_$ARCH.deb/"`
+			fi
+			debu=`echo $oldurl | sed "s/.*+deb[0-9]\{1,2\}u\([0-9]\{1,\}\)_$ARCH.deb/\1/"`
+			if [ "$debu" == "" ]
+			then
+				echo "Unable to parse URL $oldurl to attempt automated guessing"
+				exit 1
+			else
+				ATTEMPTS=1
+				while [ $ATTEMPTS -lt $MAX_DEBU_ATTEMPTS ]
+				do
+					newdebu=$((debu + ATTEMPTS))
+					# try and replace the last revision number of the debXuY
+					newurl=`echo $oldurl | sed "s/u${debu}_$ARCH.deb.*/u${newdebu}_$ARCH.deb/"`
+					echo Retrying: $newurl
+					curl -s -w '%{filename_effective} %{url_effective} %{http_code}\n' -LO $newurl > $ONE_LINE_LOG
+					cat $ONE_LINE_LOG | grep " 404" || { echo SUCCESS; break; }
+					oldfile=`echo $line | cut -d" " -f 1`
+					rm -rf $oldfile
+					ATTEMPTS=$(( ATTEMPTS + 1 ))
+				done
+			fi
+			echo
+		fi
+	done < $TMP_LOG
+fi
+rm -rf $TMP_LOG $ONE_LINE_LOG
 ls $TMP_DIR/* 2>/dev/null || exit 0
 rsync -a $TMP_DIR $BBB_ADDRESS:$TMP_DIR
 ssh $BBB_ADDRESS "dpkg -i $TMP_DIR/*"
